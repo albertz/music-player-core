277c0c5139749a8588cef8b88c8fb785fe9774c1
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-01-30 13:18:37 +0100

    module classifiers

--- setup.py
+++ setup.py
@@ -27,6 +27,28 @@ setup(
 	url = 'https://github.com/albertz/music-player-core',
 	license = '2-clause BSD license',
 	long_description = open('README.rst').read(),
+	classifiers = [
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Environment :: MacOS X',
+		'Environment :: Win32 (MS Windows)',
+		'Environment :: X11 Applications',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education',
+		'Intended Audience :: End Users/Desktop',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Operating System :: Unix',
+		'Programming Language :: C++',
+		'Programming Language :: Python'
+		'Topic :: Multimedia :: Sound/Audio',
+		'Topic :: Multimedia :: Sound/Audio :: Analysis',
+		'Topic :: Multimedia :: Sound/Audio :: Players',
+		'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		],
 	ext_modules = [mod]
 )
 
