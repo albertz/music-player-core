eab6674716d9b6bce5607b1fa719da0aee1fec40
Albert Zeyer <albzey@gmail.com>
2018-10-02 23:55:31 +0200

    PyUtils, str/unicode updates for Python 3

--- PyUtils.h
+++ PyUtils.h
@@ -29,29 +29,49 @@ std::string objStr(PyObject* obj);
 static inline
 bool pyStr(PyObject* obj, std::string& str) {
 	if(!obj) return false;
+#if PY_MAJOR_VERSION == 2
 	if(PyString_Check(obj)) {
 		str = std::string(PyString_AS_STRING(obj), PyString_GET_SIZE(obj));
 		return true;
 	}
+#else
+	if(PyBytes_Check(obj)) {
+		str = std::string(PyBytes_AS_STRING(obj), PyBytes_GET_SIZE(obj));
+		return true;
+	}
+#endif
 	else if(PyUnicode_Check(obj)) {
 		PyObject* strObj = PyUnicode_AsUTF8String(obj);
 		if(!strObj) return false;
 		bool res = false;
+#if PY_MAJOR_VERSION == 2
 		if(PyString_Check(strObj))
+#else
+		if(PyBytes_Check(strObj))
+#endif
 			res = pyStr(strObj, str);
 		Py_DECREF(strObj);
 		return res;
 	}
 	else {
+#if PY_MAJOR_VERSION == 2
 		PyObject* unicodeObj = PyObject_Unicode(obj);
+#else
+		PyObject* unicodeObj = PyObject_Str(obj);
+#endif
 		if(!unicodeObj) return false;
 		bool res = false;
-		if(PyString_Check(unicodeObj) || PyUnicode_Check(unicodeObj))
+#if PY_MAJOR_VERSION == 2
+		if(PyString_Check(unicodeObj))
+			res = pyStr(unicodeObj, str);
+		else
+#endif
+		if(PyUnicode_Check(unicodeObj))
 			res = pyStr(unicodeObj, str);
 		Py_DECREF(unicodeObj);
 		return res;
 	}
-	return false;	
+	return false;
 }
 
 #endif
