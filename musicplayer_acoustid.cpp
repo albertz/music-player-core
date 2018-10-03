e7f026b505239484bdf0322dbc22e43475f1e7d7
Albert Zeyer <albzey@gmail.com>
2018-10-03 17:58:33 +0200

    Python 3

--- musicplayer_acoustid.cpp
+++ musicplayer_acoustid.cpp
@@ -12,12 +12,12 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 	PyObject* songObj = NULL;
 	if(!PyArg_ParseTuple(args, "O:calcAcoustIdFingerprint", &songObj))
 		return NULL;
-	
+
 	PyObject* returnObj = NULL;
 	PlayerObject* player = NULL;
 	ChromaprintContext *chromaprint_ctx = NULL;
 	unsigned long totalFrameCount = 0;
-	
+
 	player = (PlayerObject*) pyCreatePlayer(NULL);
 	if(!player) goto final;
 	player->lock.enabled = false;
@@ -29,13 +29,13 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 	player->curSong = songObj;
 	if(!player->openInStream()) goto final;
 	if(PyErr_Occurred()) goto final;
-	
+
 	// fpcalc source for reference:
 	// https://github.com/lalinsky/chromaprint/blob/master/examples/fpcalc.c
-	
+
 	chromaprint_ctx = chromaprint_new(CHROMAPRINT_ALGORITHM_DEFAULT);
 	chromaprint_start(chromaprint_ctx, player->outSamplerate, player->outNumChannels);
-	
+
 	// Note that we don't have any max_length handling yet.
 	// fpcalc uses a default of 120 seconds.
 	// This function right now doesn't rely on any external song duration
@@ -48,12 +48,12 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 	// Maybe we can optimize audio_decode_frame though to just return the
 	// len and don't do any decoding if we just want to calculate the len.
 	// This is all open for future hacking ... But it works good enough now.
-	
+
 	while(player->processInStream()) {
 		if(PyErr_Occurred()) goto final;
 		for(auto& it : player->inStreamBuffer()->chunks) {
 			totalFrameCount += it.size() / player->outNumChannels / OUTSAMPLEBYTELEN;
-		
+
 			// chromaprint expects sint16 sample format.
 			int16_t pcmBuffer[BUFFER_CHUNK_SIZE/OUTSAMPLEBYTELEN];
 			uint16_t len = it.size() / OUTSAMPLEBYTELEN;
@@ -61,7 +61,7 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 				OUTSAMPLE_t s = ((OUTSAMPLE_t*)it.pt())[i];
 				pcmBuffer[i] = FloatToPCM16(OutSampleAsFloat(s));
 			}
-			
+
 			if (!chromaprint_feed(chromaprint_ctx, pcmBuffer, len)) {
 				PyErr_SetString(PyExc_RuntimeError, "fingerprint feed calculation failed");
 				goto final;
@@ -78,7 +78,7 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 	{
 		double songDuration = (double)totalFrameCount / player->outSamplerate;
 		char* fingerprint = NULL;
-		
+
 		if (!chromaprint_finish(chromaprint_ctx)) {
 			PyErr_SetString(PyExc_RuntimeError, "fingerprint finish calculation failed");
 			goto final;
@@ -88,14 +88,18 @@ pyCalcAcoustIdFingerprint(PyObject* self, PyObject* args) {
 			PyErr_SetString(PyExc_RuntimeError, "unable to calculate fingerprint, get_fingerprint failed");
 			goto final;
 		}
-		
+
 		returnObj = PyTuple_New(2);
 		PyTuple_SetItem(returnObj, 0, PyFloat_FromDouble(songDuration));
+#if PY_MAJOR_VERSION == 2
 		PyTuple_SetItem(returnObj, 1, PyString_FromString(fingerprint));
-		
+#else
+		PyTuple_SetItem(returnObj, 1, PyUnicode_FromString(fingerprint));
+#endif
+
 		chromaprint_dealloc(fingerprint);
 	}
-	
+
 final:
 	if(chromaprint_ctx)
 		chromaprint_free(chromaprint_ctx);
