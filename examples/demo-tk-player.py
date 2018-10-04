597274730757127827a810d0d7bf2ff9c395fe3f
Albert Zeyer <albzey@gmail.com>
2018-10-04 23:10:52 +0200

    Python 3, Python code style

--- examples/demo-tk-player.py
+++ examples/demo-tk-player.py
@@ -1,89 +1,122 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-import sys, os, fnmatch, random, pprint, Tkinter
+from __future__ import print_function
+import sys
+import os
+import fnmatch
+import random
+import pprint
+import tkinter
 
 # Our parent path might contain a self-build musicplayer module. Use that one.
-sys.path = [os.path.abspath((os.path.dirname(__file__) or ".") + "/..")] + sys.path
+sys.path.insert(0, os.path.abspath((os.path.dirname(__file__) or ".") + "/.."))
 
 import musicplayer
-print "Module:", musicplayer.__file__
+
+print("Module:", musicplayer.__file__)
+
 
 class Song:
-	def __init__(self, fn):
-		self.url = fn
-		self.f = open(fn)
-	# `__eq__` is used for the peek stream management
-	def __eq__(self, other):
-		return self.url == other.url
-	# this is used by the player as the data interface
-	def readPacket(self, bufSize):
-		return self.f.read(bufSize)
-	def seekRaw(self, offset, whence):
-		r = self.f.seek(offset, whence)
-		return self.f.tell()
+    def __init__(self, fn):
+        self.url = fn
+        self.f = open(fn, "rb")
+
+    # `__eq__` is used for the peek stream management
+    def __eq__(self, other):
+        return self.url == other.url
+
+    # this is used by the player as the data interface
+    def readPacket(self, bufSize):
+        return self.f.read(bufSize)
+
+    def seekRaw(self, offset, whence):
+        r = self.f.seek(offset, whence)
+        return self.f.tell()
+
 
 files = []
-def getFiles(path):
-	for f in sorted(os.listdir(path), key=lambda k: random.random()):
-		f = os.path.join(path, f)
-		if os.path.isdir(f): getFiles(f) # recurse
-		if len(files) > 1000: break # break if we have enough
-		if fnmatch.fnmatch(f, '*.mp3'): files.append(f)
-getFiles(os.path.expanduser("~/Music"))
-random.shuffle(files) # shuffle some more
+
+
+def get_files(path):
+    for f in sorted(os.listdir(path), key=lambda k: random.random()):
+        f = os.path.join(path, f)
+        if os.path.isdir(f):
+            get_files(f)  # recurse
+        if len(files) > 1000:
+            break  # break if we have enough
+        if fnmatch.fnmatch(f, '*.mp3'):
+            files.append(f)
+
+
+get_files(os.path.expanduser("~/Music"))
+random.shuffle(files)  # shuffle some more
 files = sys.argv[1:] + files
 assert files, "give me some files or fill-up ~/Music"
 
 i = 0
 
-def songs():
-	global i, files
-	while True:
-		yield Song(files[i])
-		i += 1
-		if i >= len(files): i = 0
 
-def peekSongs(n):
-	nexti = i + 1
-	if nexti >= len(files): nexti = 0
-	return map(Song, (files[nexti:] + files[:nexti])[:n])
+def songs():
+    global i, files
+    while True:
+        yield Song(files[i])
+        i += 1
+        if i >= len(files): i = 0
+
+
+def peek_songs(n):
+    next_i = i + 1
+    if next_i >= len(files):
+        next_i = 0
+    return map(Song, (files[next_i:] + files[:next_i])[:n])
+
 
 # Create our Music Player.
 player = musicplayer.createPlayer()
-player.outSamplerate = 96000 # support high quality :)
+player.outSamplerate = 96000  # support high quality :)
 player.queue = songs()
-player.peekQueue = peekSongs
+player.peekQueue = peek_songs
 
 # Setup a simple GUI.
-window = Tkinter.Tk()
-window.title("Music Player")
-songName = Tkinter.StringVar()
-songTime = Tkinter.StringVar()
-songLabel = Tkinter.StringVar()
+tk_window = tkinter.Tk()
+tk_window.title("Music Player")
+tk_song_name = tkinter.StringVar()
+tk_song_time = tkinter.StringVar()
+tk_song_label = tkinter.StringVar()
 
-def onSongChange(**kwargs):
-	songName.set(os.path.basename(player.curSong.url))
-	songLabel.set(pprint.pformat(player.curSongMetadata))
-def cmdPlayPause(*args): player.playing = not player.playing
-def cmdSeekBack(*args): player.seekRel(-10)
-def cmdSeekFwd(*args): player.seekRel(10)
-def cmdNext(*args): player.nextSong()
 
-def refreshTime():
-	songTime.set("Time: %.1f / %.1f" % (player.curSongPos or -1, player.curSongLen or -1))
-	window.after(10, refreshTime) # every 10ms
+def on_song_change(**kwargs):
+    tk_song_name.set(os.path.basename(player.curSong.url))
+    tk_song_label.set(pprint.pformat(player.curSongMetadata))
 
-Tkinter.Button(window, text="Play/Pause", command=cmdPlayPause).pack()
-Tkinter.Button(window, text="Seek -10", command=cmdSeekBack).pack()
-Tkinter.Button(window, text="Seek +10", command=cmdSeekFwd).pack()
-Tkinter.Button(window, text="Next", command=cmdNext).pack()
-Tkinter.Label(window, textvariable=songName).pack()
-Tkinter.Label(window, textvariable=songTime).pack()
-Tkinter.Label(window, textvariable=songLabel).pack()
 
-refreshTime()
+def cmd_play_pause(*args): player.playing = not player.playing
 
-player.onSongChange = onSongChange
-player.playing = True # start playing
-window.mainloop()
 
+def cmd_seek_back(*args): player.seekRel(-10)
+
+
+def cmd_seek_fwd(*args): player.seekRel(10)
+
+
+def cmd_next(*args): player.nextSong()
+
+
+def refresh_time():
+    tk_song_time.set("Time: %.1f / %.1f" % (player.curSongPos or -1, player.curSongLen or -1))
+    tk_window.after(10, refresh_time)  # every 10ms
+
+
+tkinter.Button(tk_window, text="Play/Pause", command=cmd_play_pause).pack()
+tkinter.Button(tk_window, text="Seek -10", command=cmd_seek_back).pack()
+tkinter.Button(tk_window, text="Seek +10", command=cmd_seek_fwd).pack()
+tkinter.Button(tk_window, text="Next", command=cmd_next).pack()
+tkinter.Label(tk_window, textvariable=tk_song_name).pack()
+tkinter.Label(tk_window, textvariable=tk_song_time).pack()
+tkinter.Label(tk_window, textvariable=tk_song_label).pack()
+
+refresh_time()
+
+player.onSongChange = on_song_change
+player.playing = True  # start playing
+tk_window.mainloop()
